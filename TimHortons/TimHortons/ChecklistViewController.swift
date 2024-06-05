9ef9872d97f87b8d96d6182e3ef7182447b2cde1
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-06-04 23:38:06 -0400

    Initial files

---
+++ TimHortons/TimHortons/ChecklistViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimHortons
+//
+//  Created by Alireza Khibari on 2024-06-04.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
