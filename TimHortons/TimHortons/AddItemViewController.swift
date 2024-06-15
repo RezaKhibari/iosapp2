7df1d4fc9b9489e4cf7dea3d0c876804b82deec4
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-06-15 09:19:16 -0400

    End of the Chapter 12

--- TimHortons/TimHortons/AddItemViewController.swift
+++ TimHortons/TimHortons/AddItemViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-  override func viewDidLoad() {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+   
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
       navigationItem.largeTitleDisplayMode = .never
   }
@@ -19,7 +25,44 @@ class AddItemViewController: UITableViewController {
     }
 
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
       navigationController?.popViewController(animated: true)
     }
 
+    // MARK: - Table View Delegates
+    override func tableView(
+      _ tableView: UITableView,
+      willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+      return nil
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      textField.becomeFirstResponder()
+    }
+
+    // MARK: - Text Field Delegates
+    func textField(
+      _ textField: UITextField,
+      shouldChangeCharactersIn range: NSRange,
+      replacementString string: String
+    ) -> Bool {
+      let oldText = textField.text!
+      let stringRange = Range(range, in: oldText)!
+      let newText = oldText.replacingCharacters(
+        in: stringRange,
+        with: string)
+      if newText.isEmpty {
+        doneBarButton.isEnabled = false
+      } else {
+        doneBarButton.isEnabled = true
+      }
+      return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+      doneBarButton.isEnabled = false
+      return true
+    }
 }
