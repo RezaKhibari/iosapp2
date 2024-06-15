f703729305fa4bd1c0bbfa92d9d2183939bdd585
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-06-15 10:34:50 -0400

    End of the Chapter 13

--- TimHortons/TimHortons/AddItemViewController.swift
+++ TimHortons/TimHortons/AddItemViewController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
+protocol AddItemViewControllerDelegate: AnyObject {
+  func addItemViewControllerDidCancel(
+    _ controller: AddItemViewController)
+  func addItemViewController(
+    _ controller: AddItemViewController,
+    didFinishAdding item: ChecklistItem
+  )
+}
+
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
-   
     @IBOutlet weak var textField: UITextField!
+    
+    weak var delegate: AddItemViewControllerDelegate?
+
     
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,12 +32,14 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
     // MARK: - Actions
     @IBAction func cancel() {
-      navigationController?.popViewController(animated: true)
+      delegate?.addItemViewControllerDidCancel(self)
     }
 
     @IBAction func done() {
-        print("Contents of the text field: \(textField.text!)")
-      navigationController?.popViewController(animated: true)
+      let item = ChecklistItem()
+      item.text = textField.text!
+
+      delegate?.addItemViewController(self, didFinishAdding: item)
     }
 
     // MARK: - Table View Delegates
--- TimHortons/TimHortons/ChecklistViewController.swift
+++ TimHortons/TimHortons/ChecklistViewController.swift
@@ -7,7 +7,22 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem) {
+        let newRowIndex = items.count
+          items.append(item)
+
+          let indexPath = IndexPath(row: newRowIndex, section: 0)
+          let indexPaths = [indexPath]
+          tableView.insertRows(at: indexPaths, with: .automatic)
+          navigationController?.popViewController(animated:true)
+    }
+    
+
     var items = [ChecklistItem]()
     
     override func viewDidLoad() {
@@ -94,17 +109,7 @@ class ChecklistViewController: UITableViewController {
     }
 
     // MARK: - Actions
-    @IBAction func addItem() {
-        let newRowIndex = items.count
-
-          let item = ChecklistItem()
-          item.text = "I am a new row"
-          items.append(item)
-
-          let indexPath = IndexPath(row: newRowIndex, section: 0)
-          let indexPaths = [indexPath]
-          tableView.insertRows(at: indexPaths, with: .automatic)
-    }
+   
     
     override func tableView(
       _ tableView: UITableView,
@@ -117,6 +122,20 @@ class ChecklistViewController: UITableViewController {
       // 2
       let indexPaths = [indexPath]
       tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+
+    // MARK: - Navigation
+    override func prepare(
+      for segue: UIStoryboardSegue,
+      sender: Any?
+    ) {
+      // 1
+      if segue.identifier == "AddItem" {
+        // 2
+        let controller = segue.destination as! AddItemViewController
+        // 3
+        controller.delegate = self
+      }
     }
 
 }
