4972400132ab02f8576c6dbae3abce3a458171f8
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-06-19 09:23:32 -0400

    End of chapter 15

--- TimHortons/TimHortons/ChecklistViewController.swift
+++ TimHortons/TimHortons/ChecklistViewController.swift
@@ -12,27 +12,29 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
         navigationController?.popViewController(animated: true)
     }
     
-    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem) {
+    func itemDetailViewController(_ controller: ItemDetailViewController, 
+           didFinishAdding item: ChecklistItem) {
+        
            let newRowIndex = items.count
            items.append(item)
-
            let indexPath = IndexPath(row: newRowIndex, section: 0)
            let indexPaths = [indexPath]
            tableView.insertRows(at: indexPaths, with: .automatic)
-   
-          navigationController?.popViewController(animated:true)
+           navigationController?.popViewController(animated:true)
+           saveChecklistItems()
     }
-    func itemDetailViewController(
-      _ controller: ItemDetailViewController,
-      didFinishEditing item: ChecklistItem
-    ) {
-      if let index = items.firstIndex(of: item) {
+    
+    func itemDetailViewController(_ controller: ItemDetailViewController,
+      didFinishEditing item: ChecklistItem) {
+        
+        if let index = items.firstIndex(of: item) {
         let indexPath = IndexPath(row: index, section: 0)
         if let cell = tableView.cellForRow(at: indexPath) {
           configureText(for: cell, with: item)
         }
       }
       navigationController?.popViewController(animated: true)
+      saveChecklistItems()
     }
 
 
@@ -40,30 +42,9 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationController?.navigationBar.prefersLargeTitles = true
-
-        
-        let item1 = ChecklistItem()
-        item1.text = "Dark Roast regular medium size coffee"
-        items.append(item1)
-        
-        let item2 = ChecklistItem()
-        item2.text = "Decaf double double large size coffee"
-        item2.checked = true
-        items.append(item2)
-        
-        let item3 = ChecklistItem()
-        item3.text = "Original Blend black small size coffee"
-        item3.checked = true
-        items.append(item3)
-        
-        let item4 = ChecklistItem()
-        item4.text = "Mocha Latte medium size"
-        items.append(item4)
-        
-        let item5 = ChecklistItem()
-        item5.text = "Vanilla Latte large size"
-        items.append(item5)
+        loadChecklistItems()
     }
     
     // MARK: - Table View Data Source
@@ -160,6 +141,53 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
             controller.itemToEdit = items[indexPath.row]
           }
         }
+    }
+
+    func documentsDirectory() -> URL {
+      let paths = FileManager.default.urls(
+        for: .documentDirectory,
+        in: .userDomainMask)
+      return paths[0]
+    }
+
+    func dataFilePath() -> URL {
+      return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+
+    func saveChecklistItems() {
+      // 1
+      let encoder = PropertyListEncoder()
+      // 2
+      do {
+        // 3
+        let data = try encoder.encode(items)
+        // 4
+        try data.write(
+          to: dataFilePath(),
+          options: Data.WritingOptions.atomic)
+        // 5
+      } catch {
+        // 6
+        print("Error encoding item array: \(error.localizedDescription)")
+      }
+    }
+
+    func loadChecklistItems() {
+      // 1
+      let path = dataFilePath()
+      // 2
+      if let data = try? Data(contentsOf: path) {
+        // 3
+        let decoder = PropertyListDecoder()
+        do {
+          // 4
+          items = try decoder.decode(
+            [ChecklistItem].self,
+            from: data)
+        } catch {
+          print("Error decoding item array: \(error.localizedDescription)")
+        }
+      }
     }
 
 }
